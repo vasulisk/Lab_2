4f58ff7ab6242dfb2b89859ab73f22ab310a7079
vasulisk <99254109+vasulisk@users.noreply.github.com>
2022-02-22 20:56:27 +0200

    Add files via upload

---
+++ Lab_2/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 6;
+        System.out.println("Результат ділення чисел a та b: " + a / b);
+        System.out.println("Залишок від ділення чисел a та b: " + a % b );
+
+    }
+}
